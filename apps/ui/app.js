e8f0fc29d290aee9e0f05917095bd47e60727683
Chad Riedeman <chad.riedeman@gmail.com>
2025-07-12 17:16:45 -0500

    small changes

--- apps/ui/app.js
+++ apps/ui/app.js
@@ -661,7 +661,7 @@ function getIndividualWorkoutHistoryExerciseHtml(exercise, exerciseNumber) {
     setDiv.style.display = 'table';
 
     setDiv.innerHTML = `
-      <h3 class="set-heading">Set ${setNumber} <button class="workout-set-note-button">&#128221</button> ${exerciseNumber === 1 ? `` : `<button onclick="deleteSet()" class="delete-button"> X </button>`}</h3>
+      <h3 class="set-heading">Set ${setNumber} <button class="workout-set-note-button">&#128221</button> ${setNumber === 1 ? `` : `<button onclick="deleteSet()" class="delete-button"> X </button>`}</h3>
       <div class="form-group" style="display: table-row;">
         <label style="display: table-cell; text-align: right;">Weight:</label>
         <input id="history-weight-input-${exerciseNumber}-${setNumber}" type="number" step="any" class="log-workout-set-inputs" style="display: table-cell;" value="${set.weight}">
